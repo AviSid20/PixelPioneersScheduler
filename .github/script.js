db4e0e66a3e4f194e62163ec73ebc12b4ab7c79c
Eman Elmahi <elmahie@myumanitoba.ca>
2024-04-02 11:11:17 -0500

    Added videos, play button, footer, white background behind text to make it easier to see

--- .github/script.js
+++ .github/script.js
@@ -1,10 +1,1 @@
-// const scrollText = document.querySelector('.scroll-text');
-
-// window.addEventListener('scroll', () => {
-//     const scrollPosition = window.scrollY;
-
-//     // Adjust the value based on when you want the text to appear
-//     if (scrollPosition > 100) {
-//         scrollText.classList.add('show');
-//     }
-// });
+$(document).ready(function(){$(body).css('display','block')});
