25ab952984eada14b7349f108db338bd3e438042
Eman Elmahi <elmahie@myumanitoba.ca>
2024-04-01 22:04:29 -0500

    Added download Link; Added some writing

---
+++ .github/script.js
@@ -0,0 +1,10 @@
+// const scrollText = document.querySelector('.scroll-text');
+
+// window.addEventListener('scroll', () => {
+//     const scrollPosition = window.scrollY;
+
+//     // Adjust the value based on when you want the text to appear
+//     if (scrollPosition > 100) {
+//         scrollText.classList.add('show');
+//     }
+// });
